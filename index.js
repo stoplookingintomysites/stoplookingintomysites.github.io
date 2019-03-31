75f329a93689c351f0aeffbfd57e971001c6a7c5
Justin <anicegamr@gmail.com>
2019-03-31 12:04:48 -0400

    Rough test

---
+++ index.js
@@ -0,0 +1,26 @@
+function obj(id){
+	return document.getElementById(id);
+}
+obj("causes").onclick = function() {
+	location.href = "causes.html";
+}
+
+obj("location").onclick = function() {
+	location.href = "location.html";
+}
+
+obj("protection").onclick = function() {
+	location.href = "protection.html";
+}
+
+obj("survival").onclick = function() {
+	location.href = "survival.html";
+}
+
+obj("riskesc").onclick = function() {
+	location.href = "riskesc.html";
+}
+
+obj("title").onclick = function() {
+	location.href = "index.html";
+}
