d7250830f18ddb4fb132ce65fae077e4206b6a0a
Justin <anicegamr@gmail.com>
2019-04-06 16:32:09 -0400

    everything but sources

--- index.js
+++ index.js
@@ -24,3 +24,25 @@ obj("riskesc").onclick = function() {
 obj("title").onclick = function() {
 	location.href = "index.html";
 }
+
+var h = window.innerHeight;
+obj("title").style.fontSize = 0.05296610169*h + 'px';
+obj("stitle").style.fontSize = 0.03707627119*h + 'px';
+if(obj("text")) {
+	obj("text").style.fontSize = 0.03707627119*h + 'px';
+}
+for(var i = 0; i < document.getElementsByClassName("button").length; i ++) {
+	document.getElementsByClassName("button")[i].style.fontSize = 0.04237288136*h + 'px';
+}
+
+window.onresize = function() {
+	h = window.innerHeight;
+	obj("title").style.fontSize = 0.05296610169*h + 'px';
+	obj("stitle").style.fontSize = 0.03707627119*h + 'px';
+	if(obj("text")) {
+		obj("text").style.fontSize = 0.03707627119*h + 'px';
+	}
+	for(var i = 0; i < document.getElementsByClassName("button").length; i ++) {
+		document.getElementsByClassName("button")[i].style.fontSize = 0.04237288136*h + 'px';
+	}
+}
